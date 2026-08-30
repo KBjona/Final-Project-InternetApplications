1149b6499df390b4a28559e68dc83ce8898fc767
trt111 <tohar05022010@gmail.com>
2026-08-30 13:39:07 +0300

    fixed try edge cases

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -59,11 +59,6 @@ exports.update_items_quantities = async (req, res) => {
     try {
         items = items.filter(item => item.quantity > 0);
         const result = await Cart.UpdateItemsByMail(mail, items);
-        if (result.matchedCount === 0) { //user had no cart
-            return res.status(400).json({ message: 'No user found with that email address' });
-        } else if (result.modifiedCount === 0) {//the items field didn't exist or was already deleted
-            return res.status(200).json({ message: 'User found, but the field did not exist or was already removed' });
-        }
         res.status(200).json({ message: 'Cart changed successfully' });
     } catch (err) { // server error
         console.error(err);
@@ -73,7 +68,7 @@ exports.update_items_quantities = async (req, res) => {
 
 exports.add_item_to_cart = async (req, res) => {
     let { _id, name, price } = req.body; // get the email and items from the request's body
-    if (!_id || !name || !price) { //if there is no email or no name or no price
+    if (!_id || !name || price === undefined || price === null || price === '') { //if there is no email or no name or no price
         return res.status(400).json({ message: 'Email/name/price cannot be empty' });
     }
     if (!(req.session?.user?.mail)) { //if there is no user connected
@@ -99,7 +94,7 @@ exports.get_sccn = async (req, res) => {
     try {
         const user = await User.findByMail(mail);
         if (!user) { // if no user was found
-            return res.status(417).json({ message: 'No user with this mail'});
+            return res.status(400).json({ message: 'No user with this mail'});
         }
         if (!user.sccn) { // if the user has no saved cc
             return res.status(201).json({ message: 'No stored credit card'});
@@ -124,12 +119,7 @@ exports.update_sccn = async (req, res) => {
     }
 
     try {
-        const result = User.UpdateCCByMail(mail,new_sccn);
-        if (result.matchedCount === 0) { // didn't find user with that email
-            return res.status(400).json({ message: 'No user found with that email address'});
-        } else if (result.modifiedCount === 0) {//the sccn field we found was already like after the update
-            return res.status(200).json({ message: 'User found, but the sccn field was already the same as we entered'});
-        }
+        const result = await User.UpdateCCByMail(mail,new_sccn);
         res.status(200).json({ message: 'SCCN changed successfully'});
     } catch (err) { // server error
         console.error(err);
